7bc71bf7e4e011af3394fa508ddcb36f228c2ab7
Jay Holman <jay@wistia.com>
2017-10-24 15:35:23 -0400

    Create line items for carts with associated relationships and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,23 @@
 class Product < ApplicationRecord
-validates :title, :description, :image_url, presence: true
-validates :price, numericality: {greater_than_or_equal_to: 0.01, message: "That's not a positive number..."}
-validates :title, uniqueness: true, length: { minimum: 10 }
-validates :image_url, allow_blank: true, format: {
-  with: %r{\.(gif|jpg|png)\Z}i,
-  message: 'must be a URL for GIF, JPG, or PNG image.'
-}
+  has_many :line_items
 
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01, message: "That's not a positive number..."}
+  validates :title, uniqueness: true, length: { minimum: 10 }
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image.'
+  }
+
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
