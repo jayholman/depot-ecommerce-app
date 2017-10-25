f299d8c8017e4dcf690ca343052ff5490a739370
Jay Holman <jay@wistia.com>
2017-10-25 16:38:32 -0400

    Add product price to line items

---
+++ db/migrate/20171025202738_add_price_to_line_items.rb
@@ -0,0 +1,13 @@
+class AddPriceToLineItems < ActiveRecord::Migration[5.0]
+  def up
+    add_column :line_items, :price, :decimal, precision: 8, scale: 2
+
+    LineItem.all.each do |item|
+      item.update_attribute(:price, item.product.price)
+    end
+  end
+
+  def down
+    remove_column :line_items, :price
+  end
+end
