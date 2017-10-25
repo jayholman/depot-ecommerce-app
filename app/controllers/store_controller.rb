0be8462a4288dfab9cfaec017d92d7933aef81fd
Jay Holman <jay@wistia.com>
2017-10-25 11:41:12 -0400

    Add session counter to store index

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @counter = increment_counter
   end
+
+  private
+    def increment_counter
+      session[:counter] ||=0
+      session[:counter] += 1
+      if session[:counter] > 5
+        return session[:counter]
+      end
+    end
 end
