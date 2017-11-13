35a3a7a8522ddf3623eec19bdc7d464b0c94b17b
Jay Holman <jay@wistia.com>
2017-11-13 16:10:32 -0500

    Starting out locale options

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,12 @@ Rails.application.routes.draw do
   resources :line_items do
     post 'decrement', on: :member
   end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    rot 'store#index', as: 'store_index', via: :all
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
